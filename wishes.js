01b3123b40a937143c5e66e30a2289837967fdb6
JaiChavan999 <xtremejai@gmail.com>
2026-01-22 16:46:38 +0530

    Update wishes.js

--- wishes.js
+++ wishes.js
@@ -1,14 +1,14 @@
 const wishes = [
   {
-    "name": "Loganathan",
-    "message": "Happy anniversary"
+    "name": "Your Colleague",
+    "message": "Happy Anniversary! It's been great working with you."
   },
   {
-    "name": "Ajin",
-    "message": "Happy birthday"
+    "name": "Another Friend",
+    "message": "Congratulations on another amazing year!"
   },
   {
-    "name": "Prabakaran",
-    "message": "Have a wonderful year"
+    "name": "The Team",
+    "message": "We are so lucky to have you. Cheers!"
   }
 ];
