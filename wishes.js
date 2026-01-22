dca808d3be8d23d18836d8a96d6dee94270c08f1
JaiChavan999 <xtremejai@gmail.com>
2026-01-22 13:30:53 +0530

    Add files via upload

---
+++ wishes.js
@@ -0,0 +1,14 @@
+const wishes = [
+  {
+    "name": "Loganathan",
+    "message": "Happy anniversary"
+  },
+  {
+    "name": "Ajin",
+    "message": "Happy birthday"
+  },
+  {
+    "name": "Prabakaran",
+    "message": "Have a wonderful year"
+  }
+];
